5bbe88057a2efd813ee598512b4bc34b0da74878
Fin157 <444stuart@gmail.com>
2023-03-07 14:23:46 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    public char[] stack;
+
+    // Cmd parameter 1: path leading to file with Chimpfuck program
+    private static void Main(string[] args)
+    {
+        
+    }
+}
