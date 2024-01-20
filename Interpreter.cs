06c4557fc581e0760a8c8b7b60d5302c8b65b884
Fin157 <444stuart@gmail.com>
2024-01-20 13:06:28 +0100

    Added all changed made during the programming competition 2023.

---
+++ Interpreter.cs
@@ -0,0 +1,55 @@
+﻿namespace MonkeyLanguage
+{
+    internal static class Interpreter
+    {
+        private static ushort[] stack;
+        private static int pointer;
+
+        public static void Interpret(string program, int stackSize)
+        {
+            stack = new ushort[stackSize];
+            pointer = 0;
+
+            for (int i = 0; i <= program.Length / 8; i++)
+            {
+                string operation = program.Substring(i * 8, 7);
+
+                switch (operation)
+                {
+                    case "Oo. Oo?":
+                        ++pointer;
+                        break;
+                    case "Oo? Oo.":
+                        --pointer;
+                        break;
+                    case "Oo. Oo.":
+                        ++stack[pointer];
+                        break;
+                    case "Oo! Oo!":
+                        --stack[pointer];
+                        break;
+                    case "Oo! Oo.":
+                        Console.Write((char)stack[pointer]);
+                        break;
+                    case "Oo. Oo!":
+                        stack[pointer] = (ushort)Console.Read();
+                        break;
+                    case "Oo? Oo?":
+                        Console.WriteLine("Chimp gets a banana. Chimp is happy (Oo!)");
+                        break;
+                }
+
+                if (pointer < 0)
+                {
+                    Console.WriteLine("Memory underflow.");
+                    return;
+                }
+                else if (pointer > stackSize - 1)
+                {
+                    Console.WriteLine("Memory overflow.");
+                    return;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿internal class Program
+﻿using MonkeyLanguage;
+
+internal class Program
 {
-    public char[] stack;
+    public static ushort[] stack;
 
     // Cmd parameter 1: path leading to file with Chimpfuck program
+    // Cmd parameter 2: stack size (30 000 if missing)
     private static void Main(string[] args)
     {
-        
+        //Check if a file path was actually fed into the program to prevent exceptions
+        //if (args.Length < 2 || !int.TryParse(args[1], out int stackSize))
+        //{
+        //    Console.WriteLine("Invalid command line arguments.");
+        //    return;
+        //}
+
+        Interpreter.Interpret(Console.ReadLine(), int.Parse(Console.ReadLine()));
     }
 }
